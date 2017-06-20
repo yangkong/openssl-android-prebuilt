354a01479b9db758209b7706be1ace7382da48cb
Seungjun Lee <sjlee@funple.com>
2017-06-20 14:46:00 +0900

    usage
    usage

---
+++ Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcrypto
+LOCAL_SRC_FILES := jniLibs/$(TARGET_ARCH_ABI)/libcrypto.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libssl
+LOCAL_SRC_FILES := jniLibs/$(TARGET_ARCH_ABI)/libssl.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := {Lib Name}
+LOCAL_SRC_FILES := {Lib Src}
+LOCAL_C_INCLUDES := \
+	{include/openssl folder locatin}
+LOCAL_SHARED_LIBRARIES := libcrypto libssl
+LOCAL_LDLIBS := -lz -llog
+include $(BUILD_SHARED_LIBRARY)
